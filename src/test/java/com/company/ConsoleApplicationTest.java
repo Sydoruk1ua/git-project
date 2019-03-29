850ec7932ed9aff155280d144aabc3cb79095fc2
Alex Sydoruk <sydoruk1ua@gmail.com>
2019-03-29 20:47:49 +0200

    [HW-2] Added sum method

--- src/test/java/com/company/ConsoleApplicationTest.java
+++ src/test/java/com/company/ConsoleApplicationTest.java
@@ -1,5 +1,16 @@
 package com.company;
 
+import org.junit.Test;
+
+import static com.company.ConsoleApplication.sum;
+import static org.junit.Assert.assertEquals;
+
 public class ConsoleApplicationTest {
 
+    @Test
+    public void shouldReturnSum() {
+        int actual = sum(1, 2);
+        int expected = 3;
+        assertEquals(expected, actual);
+    }
 }
